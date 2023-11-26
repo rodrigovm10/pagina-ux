6adc687d066176b26c3a98b57322f5f31921c688
YeseniaMartinez24 <martinezalmayesenia904@gmail.com>
2023-11-26 17:26:19 -0600

    calendario bonis2

--- src/components/Calendario.jsx
+++ src/components/Calendario.jsx
@@ -11,8 +11,6 @@ import {
   Text,
   Stack,
   CardBody,
-  CardFooter,
-  Button,
 } from "@chakra-ui/react";
 import { Footer } from "../components/Footer.jsx";
 import { Header } from "../components/Header.jsx";
