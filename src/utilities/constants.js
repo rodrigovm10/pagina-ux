bed79eb9fe3b67a18936268fde92bc7ea34486f6
YeseniaMartinez24 <martinezalmayesenia904@gmail.com>
2023-11-26 22:20:54 -0600

    calendario bonis2

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -78,6 +78,105 @@ export const CARDS_CALENDAR = [
       'Actividades: ',
       'Presentaciones técnicas, sesión de preguntas y respuestas, y un taller práctico.'
     ]
-  }
+  },
+  {
+    id: 9,
+    title: 'Charla sobre Innovación Tecnológica',
+    date: ['Fecha: ', '13 noviembre'],
+    description: [
+      'Actividades: ',
+      'Conferencias sobre las últimas tendencias tecnológicas, paneles de discusión, y networking.'
+    ]
+  },
+  {
+    id: 10,
+    title: 'Encuentro de Juegos de Mesa',
+    date: ['Fecha: ', '14 noviembre'],
+    description: [
+      'Actividades: ',
+      'Torneos de juegos de mesa, zona de juego libre, venta de juegos y accesorios.'
+    ]
+  },
+  {
+    id: 11,
+    title: 'Simposio de Inteligencia Artificial y Machine Learning',
+    date: ['Fecha: ', '15 noviembre'],
+    description: [
+      'Actividades: ',
+      'Charlas de expertos en IA, workshops de machine learning, paneles de discusión sobre ética en IA.'
+    ]
+  },
+  {
+    id: 12,
+    title: 'Exposición de Proyectos de Energías Renovables',
+    date: ['Fecha: ', '16 noviembre'],
+    description: [
+      'Actividades: ',
+      'Presentaciones de proyectos estudiantiles, charlas sobre sostenibilidad, paneles de discusión con expertos.'
+    ]
+  },
+  {
+    id: 13,
+    title: 'Workshop sobre Blockchain y Criptomonedas',
+    date: ['Fecha: ', '17 noviembre'],
+    description: [
+      'Actividades: ',
+      'Sesiones educativas sobre blockchain, debates sobre el futuro de las criptomonedas, casos prácticos de uso.'
+    ]
+  },
+  {
+    id: 14,
+    title: 'Conmemoración del 20 de Noviembre',
+    date: ['Fecha: ', '20 noviembre'],
+    description: [
+      'Actividades: ',
+      'Nuestra universidad se une a la pausa nacional para conmemorar un día de gran importancia histórica y cultural. En esta fecha, recordamos un momento clave en nuestra historia, un tiempo de cambio y evolución que refleja el espíritu de innovación y progreso que nos caracteriza como institución. En este día, nos alejamos de las aulas y laboratorios no solo para descansar, sino también para reflexionar sobre nuestro legado y nuestra responsabilidad hacia la sociedad.'
+    ]
+  },
+  {
+    id: 15,
+    title: 'Conferencia sobre Realidad Virtual y Aumentada',
+    date: ['Fecha: ', '21 noviembre'],
+    description: [
+      'Actividades: ',
+      'Demostraciones de VR/AR, talleres de desarrollo, paneles sobre aplicaciones futuras de estas tecnologías.'
+    ]
+  },
+  {
+    id: 16,
+    title: 'Feria de Startups y Emprendimiento',
+    date: ['Fecha: ', '22 noviembre'],
+    description: [
+      'Actividades: ',
+      'Pitches de startups, sesiones de networking, talleres sobre emprendimiento tecnológico.'
+    ]
+  },
+  {
+    id: 17,
+    title: 'Hackathon de Seguridad Cibernética',
+    date: ['Fecha: ', '23 noviembre'],
+    description: [
+      'Actividades: ',
+      ' Competencias de hacking ético, talleres sobre seguridad en la red, paneles sobre tendencias en ciberseguridad.'
+    ]
+  },
+  {
+    id: 18,
+    title: 'Festival de Innovación y Tecnología',
+    date: ['Fecha: ', '24 noviembre'],
+    description: [
+      'Actividades: ',
+      ' Exposición de gadgets y tecnologías emergentes, conferencias sobre avances tecnológicos, competencias de robótica.'
+    ]
+  },
+  {
+    id: 19,
+    title: 'Torneo de eSports',
+    date: ['Fecha: ', '27 noviembre'],
+    description: [
+      'Actividades: ',
+      'Competencias de videojuegos populares, charlas sobre la industria del gaming, área de juegos libres.'
+    ]
+  },
 ]
 
